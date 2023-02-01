4951dd551ec75c481e92e9b4b0341095a71bd6e4
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-02-01 22:30:10 +0300

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React from "react";
+
+function App() {
+    <p>Loading..</p>
+    return (
+        useEffect(() => {
+            fetch( <img src="https://api.example.com/items"></img>)
+               .then(res => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setItems(result);
+                    },)
+            }  
+      
+        )
+
+    );
+}
+
+export default App;
